1f868f37069a3d7d8c4a164465c3abffa0853bae
Andrea Bellver <bellverrha@gmail.com>
2022-12-20 14:44:36 +0100

    Quinto ejercicio JS OBC

---
+++ index.js
@@ -0,0 +1,9 @@
+let height = 170;
+let heightMeters = 170 / 100;
+
+let weight = 55.3;
+
+let heightRoundedUp = Math.ceil(heightMeters);
+let weightRoundedDown = Math.floor(weight);
+
+const isTheSame = Number.MAX_VALUE === (Number.MAX_VALUE + 1);
